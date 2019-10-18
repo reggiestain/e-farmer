fa563292b40fc7c05a9bdcc0aa4253f1d98f96ae
dev5 <reggiestain@gmail.com>
2019-10-18 13:33:49 +0200

    Added dataTables

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -17,6 +17,7 @@
         <!-- Custom styles for this template-->  
         <link rel="stylesheet" type="text/css" href="{{ asset('css/sb-admin-2.css') }}">
         <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/css/bootstrap-datepicker3.css" rel="stylesheet" id="bootstrap-css">      
+        <link href="{{ asset('js/vendor/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
     </head>        
     <body id="page-top">
         <div id="wrapper">
@@ -150,6 +151,10 @@
     <script src="{{ URL::asset('js/demo/chart-area-demo.js') }}"></script>
     <script src="{{ URL::asset('js/demo/chart-pie-demo.js') }}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/js/bootstrap-datepicker.min.js"></script>
+    <script src="{{ URL::asset('js/vendor/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ URL::asset('js/vendor/datatables/dataTables.bootstrap4.js') }}"></script>
+    <!-- Page level custom scripts -->
+   <script src="{{ URL::asset('js/demo/datatables-demo.js') }}"></script>
     <script>
                                    $(document).ready(function () {
                                        $(document).on('click', '.add-u', function () {
