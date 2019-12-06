c68571ee345abb2753a286233dfa17a1d06afaaa
dev5 <reggiestain@gmail.com>
2019-12-06 12:56:30 +0200

    Form association update

--- app/Farmer.php
+++ app/Farmer.php
@@ -11,7 +11,7 @@ class Farmer extends Model {
      *
      * @var array
      */
-    protected $fillable = ['firstname','user_id','surname', 'gender', 'age', 'birth_date', 'birth_place', 'mobile', 'mobile2', 'marital_status',
+    protected $fillable = ['firstname','user_id','surname', 'gender', 'age', 'assoc','birth_date', 'birth_place', 'mobile', 'mobile2', 'marital_status',
         'age', 'number_of_children', 'number_of_dependencies', 'address', 'postal_address', 'email','profile_image'];
 
     public function user() {
