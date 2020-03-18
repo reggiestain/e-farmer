f46d7efd1dcad9a9356f7ef96a409ccdef1527c1
dev5 <reggiestain@gmail.com>
2020-03-18 14:57:33 +0200

    updated routes

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -8,8 +8,7 @@
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>Dashboard - SB Admin</title>
-        <link href="{{ asset('public/css/styles.css')}}" rel="stylesheet" />
-        <link rel="stylesheet" type="text/css" href="css/styles.css">
+        <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
         <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js" crossorigin="anonymous"></script>         
         <style>
@@ -246,13 +245,13 @@
     <!-- Bootstrap core JavaScript-->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="js/scripts.js"></script>
+        <script src="{{ asset('js/scripts.js') }}"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-        <script src="js/assets/demo/chart-area-demo.js"></script>
-        <script src="js/assets/demo/chart-bar-demo.js"></script>
-        <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
+        <script src="{{ asset('js/assets/demo/chart-area-demo.js') }}"></script>
+        <script src="{{ asset('js/assets/demo/chart-bar-demo.js') }}"></script>
+        <script src="{{ asset('https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js') }}" crossorigin="anonymous"></script>
         <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
-        <script src="js/assets/demo/datatables-demo.js"></script>
+        <script src="{{asset('js/assets/demo/datatables-demo.js') }}"></script>
     <script>
                                    $(document).ready(function () {
 
